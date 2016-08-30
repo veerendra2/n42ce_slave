fb289cd577213ebf2e7876aaf65e9210e5f39741
Veerendra <veerendra@n42inc.com>
2016-08-30 15:20:51 +0530

    Uses Contrack, netifaces module

--- docker_app_dep_perodic.py
+++ docker_app_dep_perodic.py
@@ -2,14 +2,18 @@
 Author: Networks42
 Description: Finds Dependencies between the docker containers
 '''
-import os,subprocess,time,re,json,redis,ast,traceback
-import port_dictionary
+import os,time,json,redis,ast,traceback
 import requests,datetime
+import netifaces
+import pynetfilter_conntrack
+from pybrctl import BridgeController
+#Local Dictionary
+import port_dictionary
 
 interval=300
 hostname=os.uname()[1]
 base_path=os.path.dirname(os.path.abspath(__file__))
-tenant_name=os.environ['key']
+tenant_name="carbon"  #os.environ['key']
 interface_ip_list=list()
 bridges_ip=list()
 local_container_data=dict()
@@ -22,12 +26,6 @@ delete_url="http://52.8.104.253:8161/n42-services/resources/appdiscovery/deleteC
 dependency_url="http://52.8.104.253:8161/n42-services/resources/appdiscovery/updateContainerDependency"
 event_dictionary={"start":"started","stop":"stopped"}
 config = {'host': '52.8.104.253', 'port': 6379, 'db': 0}
-docker_path="/var/lib/docker/containers"
-
-commands={"connections":"conntrack -L -p tcp | grep 'TIME_WAIT\|ESTABLISHED\|CLOSE'",
-            "interface_ips":"ip addr | grep -w inet",
-            "bridges":"brctl show",
-            "bridges_ips":"ip addr show {} | grep -w inet"}
 
 files={"tcp_stats":"/proc/net/tcp",
        "docker-info":"/var/lib/docker/containers/{}/config.v2.json",
@@ -52,27 +50,17 @@ def merge_dicts(*dict_args):
 
 def set_interface_ips():
     global interface_ip_list, bridges_ip
-    output1=execuite_cmd(commands["interface_ips"])
-    output2=execuite_cmd(commands["bridges"])
-    for line in output1.split("\n"):
-        if not line or re.search(' lo', line):
-            continue
-        ip=line.split()[1].split("/")[0]
-        interface_ip_list.append(ip)
+    brctl = BridgeController()
+    bridge_iface=list()
+    _bridge_names=list()
+    for iface in brctl.showall():
+        _bridge_names=str(iface)
+        bridges_ip.append(netifaces.ifaddresses(str(iface))[2][0]["addr"])
+    for iface1 in netifaces.interfaces():
+        iface_atri=netifaces.ifaddresses(iface1).keys()
+        if iface1 not in _bridge_names and 2 in iface_atri:
+            interface_ip_list.append(netifaces.ifaddresses(iface1)[2][0]["addr"])
     
-    for line in output2.split("\n"):
-        if not line or re.search("bridge name", line):
-            continue
-        parts=line.split()
-        if len(parts)>2:
-            try:
-                output2=execuite_cmd(commands["bridges_ips"].format(parts[0]))
-            except Exception:
-                continue
-            b_ip=output2.split()[1].split("/")[0]
-            interface_ip_list.remove(b_ip)
-            bridges_ip.append(b_ip)
-
 def get_service(port_list,label):
     try:
         if label:
@@ -84,6 +72,7 @@ def get_service(port_list,label):
             return "unknown"
 
 def set_docker_info():
+    print "Docker Info Time-->",int(round(time.time()*1000))
     global interface_ip_list, host_dicts, local_container_data, docker_info
     global_container_data=dict()
     container_id=os.listdir(files["containers"])# Get the List of Container IDS
@@ -113,37 +102,37 @@ def set_docker_info():
                     ip_addresses.append(v1["IPAddress"])
                 service_name=get_service(port_list,data["Config"]["Labels"])
                 docker_info.append({"cid":ids[:12],"cname":data["Name"].lstrip("/"),"hostname":hostname,"pid":data["State"]["Pid"],"ip":ip_addresses,"image":data["Config"]["Image"],"Tenant_Name":tenant_name,"labels":data["Config"]["Labels"],"service":service_name})
+    print "Docker Info End Time-->",int(round(time.time()*1000))
     json_data=json.dumps(docker_info)              
     r = redis.StrictRedis(**config)
     r.hset(tenant_name+"-info",hostname,global_container_data)
-    time.sleep(3)
     cluster_info=r.hgetall(tenant_name+"-info")
     for x in cluster_info.values():
         host_dicts.update(ast.literal_eval(x))
     print "=================== Docker Info ===================\n"
     print json_data
+    print "\n Sending data",int(time.time())
     response = requests.post(info_url, data=json_data,headers=headers)
+    print "\n Data Sent!",int(time.time())
     print response,"\n"
   
-def parse_contrack():
+def analyse_traffic():
     global host_dicts,local_container_data
     neighbors=set()
-    file=execuite_cmd(commands["connections"])
-    for line in file.split("\n"):
-        if line and "127.0.0.1" not in line and "tcp" in line:
-            line_list= line.split()
-            src_ip1=line_list[4].split("=")[1]
-            dst_ip1=line_list[5].split("=")[1]
-            #src_port1=line_list[6].split("=")[1]
-            dst_port1=line_list[7].split("=")[1]
-            
-            src_ip2=line_list[8].split("=")[1]
-            dst_ip2=line_list[9].split("=")[1]
-            #src_port2=line_list[10].split("=")[1]
-            #dst_port2=line_list[11].split("=")[1]
-            if src_ip1 in bridges_ip and dst_ip2 in bridges_ip:
-                continue #Packets coming from docker0(Default Gateway)
-            elif dst_ip1+":"+dst_port1 in host_dicts and src_ip1 in local_container_data:
+    container_in_dep=set()
+    ct = pynetfilter_conntrack.Conntrack()
+    for item in ct.dump_table(netifaces.AF_INET)[0]:
+        print "Start of Loop time",int(round(time.time()*1000))
+        if (str(item.orig_ipv4_src) not in bridges_ip and str(item.repl_ipv4_dst) not in bridges_ip) or "127.0.0.1"  not in str(item.orig_ipv4_src) or "127.0.0.1" not in str(item.orig_ipv4_dst):
+            src_ip1=str(item.orig_ipv4_src)
+            dst_ip1=str(item.orig_ipv4_dst)
+            #src_port1=str(item.orig_port_src)
+            dst_port1=str(item.orig_port_dst)
+            src_ip2=str(item.repl_ipv4_src)
+            dst_ip2=str(item.repl_ipv4_dst)
+            #src_port2=str(item.repl_port_src)
+            #dst_port2=str(item.repl_port_dst)
+            if dst_ip1+":"+dst_port1 in host_dicts and src_ip1 in local_container_data:
                 if src_ip1 in local_container_data and dst_ip1+":"+dst_port1 in host_dicts:
                     src_con=local_container_data[src_ip1][1]
                     dst_con=host_dicts[dst_ip1+":"+dst_port1]["cid"]
@@ -173,7 +162,7 @@ def parse_contrack():
                     else:
                         service="Unknown:{0}".format(port)
                     neighbors.add(src_con+"-"+dst_con+"-"+service)
-                           
+        print "End of Loop Time-->",int(round(time.time() * 1000))
     dependency=list()
     for key in neighbors:
         part=key.split("-")
@@ -186,7 +175,9 @@ def parse_contrack():
     json_data=json.dumps(dependency)
     print "=================== Docker Dependencies ===================\n"
     print json_data
+    print "\n Sending data to API",int(time.time())
     response = requests.post(dependency_url, data=json_data,headers=headers)
+    print "\n Data Sent!",int(time.time())
     print response
     
                   
@@ -194,7 +185,7 @@ while True:
     try:
         set_interface_ips()
         set_docker_info()
-        parse_contrack()
+        analyse_traffic()
         wipe_varibles()
         print "Completed at",datetime.datetime.now()
         time.sleep(interval)
@@ -202,6 +193,6 @@ while True:
         print "================ Something Went Wrong. TRACEBACK BELOW ================\n"
         print traceback.format_exc()
         print "\nTIMESTAMP",datetime.datetime.now(),"\n"
-        continue
+
 
 
