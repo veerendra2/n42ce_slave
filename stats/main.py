05babc2a96fd6f4d46e7157fe8a149734ab53413
Veerendra <veerendra2@users.noreply.github.com>
2016-09-12 17:01:37 +0530

    Update main.py

--- stats/main.py
+++ stats/main.py
@@ -1,19 +1,13 @@
-import interface_metrics
-import cpu
+import cpu_util
 import docker_stats
-import time,os
-import yaml,sys
+import time
+import traceback
 
-try:
-     	f = open('/opt/n42Agents/stats/config.yml', 'r')
-     	config = yaml.load(f)
-        interval = config['tsdb']['interval']#os.environ['interval']
-	hostname = os.uname()[1]
-	config['node_config_files']['host'] = hostname
-    	while True:
-		#interface_metrics.main(config)
-		cpu.main(config)
-		docker_stats.main(config)
-		time.sleep(interval)
-except Exception ,e :
-        print "Exception:",e,"At Line Number {}".format(sys.exc_info()[-1].tb_lineno)
+while True:
+    try:
+        cpu_util.main()
+        docker_stats.main()
+        time.sleep(3)
+    except Exception ,e :
+        print "=========TRACEBACK-MAIN========="
+        print traceback.format_exc()
